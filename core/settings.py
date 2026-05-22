8129393d72a4fa6e427e23f34dc1f3b3205bd9c7
Mohammad Arman Hossen <mdarmanhossen404@gmail.com>
2026-05-22 23:28:43 +0600

    Dockerized backend

--- core/settings.py
+++ core/settings.py
@@ -50,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True # For MVP
